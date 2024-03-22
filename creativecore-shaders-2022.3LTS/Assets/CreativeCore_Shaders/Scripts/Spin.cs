d4022952053c5bcf96285ba5f9b1d890c885f53a
ThuyLan22 <thuy_ritondale@studenti.naba.it>
2024-03-22 15:28:27 +0100

    Kettle

--- creativecore-shaders-2022.3LTS/Assets/CreativeCore_Shaders/Scripts/Spin.cs
+++ creativecore-shaders-2022.3LTS/Assets/CreativeCore_Shaders/Scripts/Spin.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Thuy : MonoBehaviour
+public class Spin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public float Speed;
 
-    // Update is called once per frame
     void Update()
     {
-        
+    transform.Rotate(0, Speed * Time.deltaTime, 0, Space.World);
     }
 }
