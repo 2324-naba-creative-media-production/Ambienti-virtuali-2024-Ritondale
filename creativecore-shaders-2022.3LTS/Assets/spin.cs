6ab7f8e30efb2bfa72106a88130aa9ff7784eac3
ThuyLan22 <thuy_ritondale@studenti.naba.it>
2024-04-09 11:36:01 +0200

    AlienSpin

--- creativecore-shaders-2022.3LTS/Assets/spin.cs
+++ creativecore-shaders-2022.3LTS/Assets/spin.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class spin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float spinSpeed = 50f; 
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
     }
 }
