fc0e8f4eb9f0663cd93ec16d9b211a0a6e5424c1
ThuyLan22 <thuy_ritondale@studenti.naba.it>
2024-04-12 10:07:22 +0200

    UpDown

---
+++ creativecore-shaders-2022.3LTS/Assets/SphereMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+    public float speed = 2f; 
+    public float amplitude = 0.005f;
+    public float frequency = 1f; 
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 newPosition = startPos + Vector3.up * Mathf.Sin(Time.time * frequency) * amplitude;
+
+        transform.position = newPosition;
+
+}
+}
